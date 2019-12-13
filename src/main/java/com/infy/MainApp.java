ed11ac9ad568b0682455f3560988eb0a7c8c0735
kanav <kanav.kohli@infosys.com>
2019-12-13 09:39:39 +0530

    Docker file updated

--- src/main/java/com/infy/MainApp.java
+++ src/main/java/com/infy/MainApp.java
@@ -8,7 +8,7 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-SpringApplication.run(MainApp.class, args);
+		SpringApplication.run(MainApp.class, args);
 	}
 
 }
